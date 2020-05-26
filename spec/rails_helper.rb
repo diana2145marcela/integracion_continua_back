a8159a84b26c24730b5b1d2b1a7365d9b09a8e07
davidauza.engineer <davidauza01@gmail.com>
2020-05-25 21:58:59 -0500

    Configuración adicional RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,17 +4,11 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "spec_helper"
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
-require "capybara/rspec"
 
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.javascript_driver = :selenium_chrome
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
