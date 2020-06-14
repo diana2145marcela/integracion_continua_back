3ceec58384bc105b43556b6c492221aa6159f92c
davidauza.engineer <davidauza01@gmail.com>
2020-06-14 16:35:47 -0500

    Añadir gastos a las rutas
    Se añadió gastos a las rutas y se añadieron pruenas para cada ruta.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'autenticar', to: 'authentication#autenticar'
   resources :usuarios, only: [:create, :show]
+  resources :gastos
 end
---
+++ spec/routing/gastos_routing_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe GastosController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/gastos").to route_to("gastos#index")
+    end
+
+    it "routes to #show" do
+      expect(get: "/gastos/1").to route_to("gastos#show", id: "1")
+    end
+
+
+    it "routes to #create" do
+      expect(post: "/gastos").to route_to("gastos#create")
+    end
+
+    it "routes to #update via PUT" do
+      expect(put: "/gastos/1").to route_to("gastos#update", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/gastos/1").to route_to("gastos#update", id: "1")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/gastos/1").to route_to("gastos#destroy", id: "1")
+    end
+  end
+end
